4fdcd287374d413f14bd609dffe161a4316c050a
gokul-ram-j <gokulram221@gmail.com>
2024-01-14 22:17:00 +0530

    14-1-2024

---
+++ calculator/app.js
@@ -0,0 +1,46 @@
+const btn=document.querySelectorAll("button")
+const minidisplay=document.querySelector(".minidisplay")
+const maindisplay=document.querySelector(".maindisplay")
+let olddata=false
+function result(elem){
+    if(olddata){
+        maindisplay.innerHTML=""
+        minidisplay.innerHTML=""
+        olddata=false
+    }
+    maindisplay.innerHTML+=elem
+}
+
+function display(elem){
+    if(elem=="clear"){
+        if(olddata){
+            maindisplay.innerHTML=""
+            minidisplay.innerHTML=""
+            olddata=false
+        }
+        let len=maindisplay.innerHTML.length
+        let slicedpart=maindisplay.innerHTML.substring(0,len-1)
+        if(slicedpart==undefined){
+            maindisplay.innerHTML=""
+        }
+        else{
+            maindisplay.innerHTML=slicedpart
+        }
+    }
+    if(elem=="equal"){
+        minidisplay.innerHTML=maindisplay.innerHTML
+        try{
+            let finalResult=eval(maindisplay.innerHTML)
+            finalResult=finalResult.toString()
+            maindisplay.innerHTML=finalResult
+            console.log(finalResult)
+            
+        }
+        catch(err){
+            maindisplay.innerHTML="Math Error"
+            minidisplay.innerHTML=""
+        }
+        olddata=true
+
+    }
+}
